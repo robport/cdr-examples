fab07bebc5f38604bc85469e5ea56e89489bdd10
robport <26696668+robport@users.noreply.github.com>
2024-03-09 16:47:58 +0000

    Updated Examples.

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,2 @@
-export const CDR_API_URL = 'https://customer-deposits-registry.com'
+export const CDR_API_URL = 'https://customer-deposits-registry.com';
+export const USER_EMAIL = 'robert.porter1@gmail.com';
--- src/get-auth-headers.ts
+++ src/get-auth-headers.ts
@@ -1,11 +1,12 @@
 import { randomBytes, createSign } from 'crypto';
 import fs from 'fs';
+import { USER_EMAIL } from './config';
 
 export function getAuthHeaders() {
   const messageStr = JSON.stringify({
     timestamp: new Date().toISOString(),
     randomText: randomBytes(16).toString('hex'),
-    email: 'rob@excal.tv'
+    email: USER_EMAIL
   });
 
   const privateKey = fs.readFileSync('private_key.pem', 'utf8').toString();
